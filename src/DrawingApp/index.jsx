df2a609af6635405fe99e3e811c3df820b970c63
Alexander Gatland <alexandergatland@gmail.com>
2024-06-12 10:18:53 +0200

    Add Brush changing and canvas clearing

--- src/DrawingApp/index.jsx
+++ src/DrawingApp/index.jsx
@@ -1,21 +1,86 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { fabric } from 'fabric';
+import { ActionIcon, ColorInput, Container, Slider } from '@mantine/core';
+import { IconBrush, IconCircleFilled, IconTrash } from '@tabler/icons-react';
+
+const defaultCanvas = {
+  brushSize: 5,
+  brushColor: "#000",
+  backgroundColor: "#fff"
+}
 
 const DrawingApp = () => {
-  const canvasRef = useRef(null);
+  const [canvas, setCanvas] = useState(null)
+  const canvasRef = useRef(canvas)
+
+  const [canvasSettings, setCanvasSettings] = useState(defaultCanvas)
 
   useEffect(() => {
-    const canvas = new fabric.Canvas(canvasRef.current);
-    canvas.isDrawingMode = true;
-    canvas.freeDrawingBrush.width = 5;
-    canvas.freeDrawingBrush.color = '#000';
+    const canvas = new fabric.Canvas(canvasRef.current)
+    canvas.backgroundColor = defaultCanvas.backgroundColor
+    canvas.isDrawingMode = true
+    canvas.freeDrawingBrush.width = defaultCanvas.brushSize
+    canvas.freeDrawingBrush.color = defaultCanvas.brushColor
 
+    setCanvas(canvas)
     return () => {
-      canvas.dispose();
-    };
-  }, []);
+      canvas.dispose()
+    }
+  }, [])
 
-  return <canvas ref={canvasRef} width={800} height={600}></canvas>;
+  const handleBrushSizeChange = (value) => {
+    canvas.freeDrawingBrush.width = value
+    setCanvasSettings({ ...canvasSettings, brushSize: value})
+  }
+
+  const handleBrushColorChange = (value) => {
+    canvas.freeDrawingBrush.color = value
+    setCanvasSettings({ ...canvasSettings, brushColor: value})
+  }
+
+
+  return (
+    <Container pt={100}>
+      <div style={{backgroundColor: "#fff"}}>
+        <canvas ref={canvasRef} height={600} width={928}></canvas>
+      </div>
+      <div style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}>
+        <div style={{display: "flex", flexDirection: "row", alignItems: "center", gap: "2%", flexGrow: 1}}>
+          <div>
+            <label>Brush Size</label>
+            <div style={{display: "flex", flexDirection: "row", alignItems: "center"}}>
+              <IconCircleFilled size={15}/>
+              <Slider
+                w={100}
+                value={canvasSettings.brushSize}
+                min={1}
+                max={30}
+                onChange={handleBrushSizeChange}
+              />
+              <IconCircleFilled size={25}/>
+            </div>
+          </div>
+          <div>
+            <ColorInput
+              label="Brush Color"
+              placeholder="Input placeholder"
+              value={canvasSettings.brushColor}
+              onChange={handleBrushColorChange}
+            />
+          </div>
+          <div>
+            <ActionIcon size={"xl"}>
+              <IconBrush size={30} />
+            </ActionIcon>
+          </div>
+        </div>
+        <ActionIcon size={"xl"} onClick={() => canvas.clear()}>
+          <IconTrash size={30}/>
+        </ActionIcon>
+      </div>
+    </Container>
+
+);
 };
 
 export default DrawingApp;
